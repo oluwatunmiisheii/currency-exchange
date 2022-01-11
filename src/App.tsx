c18b163cdfdcad97440d9fe4b0ff891d41e24b24
Adenuga Oluwatunmise Wilson <oluwatunmiseadenuga@gmail.com>
2022-01-11 08:20:35 +0100

    chore: add new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.module.scss';
 import CurrencyExchange from './containers/currencyExchange/CurrencyExchange';
+// import CurrencyExchangeService from './services/CurrencyExchange.service';
 
 
 interface AppProps {
@@ -8,6 +9,15 @@ interface AppProps {
 }
  
 const App: React.FC<AppProps> = () => {
+  // const processGetLatestRates = async () => {
+  //   const response = await CurrencyExchangeService.getLatestRates();
+  //   console.log(response);
+  // }
+
+  // React.useEffect(() => {
+  //   processGetLatestRates();
+  // })
+
   return (  
     <main className={styles["main-wrapper"]}>
       <CurrencyExchange />
