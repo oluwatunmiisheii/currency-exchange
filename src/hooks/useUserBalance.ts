a2312a2f5bf2eb533bf9c8e90ef90f1b5bd6efe0
Adenuga Oluwatunmise Wilson <oluwatunmiseadenuga@gmail.com>
2022-01-12 07:44:31 +0100

    chore: add custom hook to handle user account balance

---
+++ src/hooks/useUserBalance.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { Currencies } from "../enums/Currencies.enum"
+
+interface IUserAccount {
+  currency: string;
+  balance?: number;
+}
+
+interface IUserBalance extends IUserAccount {
+  amount: number | string;
+  scope: string;
+  cb: (amount: string | number, balance: number, scope: string) => void;
+}
+
+export const initialUserAccounts: IUserAccount[]  = [
+  {
+    currency: Currencies.USD,
+    balance: 100,
+  },
+  {
+    currency: Currencies.GBP,
+    balance: 0.13,
+  },
+  {
+    currency: Currencies.EUR,
+    balance: 200,
+  }
+]
+
+export default function useUserBalance(args: IUserBalance) {
+  const [userAccounts, setUserAccounts] = useState<IUserAccount[]>(initialUserAccounts);
+  const [userBalance, setUserBalance] = useState<number>(0);
+
+  const getUserBalance = ()  => {
+    let balance;
+    balance = userAccounts.find(b => b.currency === args.currency)?.balance ?? 0;
+    setUserBalance(balance);
+    console.log('running')
+    args.cb(args.amount, balance, args.scope);
+  }
+
+  useEffect (() => {
+    getUserBalance();
+  }, [args.currency])
+
+  return { setUserAccounts, userBalance  };
+}
